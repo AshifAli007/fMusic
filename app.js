544b3701fc0cc0728961dd3b70d4660f5e33f428
AshifAli007 <theashifkhan@gmail.com>
2021-05-17 19:43:16 +0530

    changes are going to live

--- app.js
+++ app.js
@@ -96,12 +96,11 @@ app.post("/add",function(req,res){
     var playlistName = (req.body.playlistName).toLowerCase();
     res.redirect("/add/"+playlistName);
 });
-app.listen(process.env.PORT||3000,function(){
-<<<<<<< HEAD
+app.listen(process.env.PORT||9000,function(){
     console.log("Server Is Running bro");
-=======
+
     console.log("Server Is Running d");
->>>>>>> responsive
+
 });
 
 
